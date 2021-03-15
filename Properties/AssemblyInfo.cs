2a9ca6c890e26d37ba122c07c1f0eb407a62cae6
ds5678 <jrpritts7@gmail.com>
2021-03-15 19:49:56 -0400

    updated assembly info

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -32,7 +32,7 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.0.0")]
-[assembly: AssemblyFileVersion("2.0.0")]
-[assembly: MelonInfo(typeof(DetectionRange.Implementation), "DetectionRange", "2.0.0", "AlexTheRegent, ds5678")]
+[assembly: AssemblyVersion("2.1.0")]
+[assembly: AssemblyFileVersion("2.1.0")]
+[assembly: MelonInfo(typeof(DetectionRange.Implementation), "DetectionRange", "2.1.0", "AlexTheRegent, ds5678")]
 [assembly: MelonGame("Hinterland", "TheLongDark")]
